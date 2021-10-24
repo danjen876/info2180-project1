ffa106b9cd2315ac7623239dabc23b7eecb09c25
Daniel Jennings <danieljennings5@gmail.com>
2021-10-24 13:54:52 -0500

    Added the final files. I worked on these files outside of the git and added them once they were complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-/* Add your JavaScript to this file */
+//Function to check if the email is valid
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+window.onload = function() //As soon as you start the window
+{
+    var button = document.querySelectorAll("button")[0];
+    
+    button.addEventListener("click",function(event) //When subscribe is clickec
+    {
+        var email = document.querySelector("input[type=email]").value;
+        var message = document.querySelector(".message");
+        
+        //Test if it's valid and print the right message
+        if (validateEmail(email)) 
+        {
+            message.innerHTML = "Thank you! Your email address has been added to our mailing list!";
+        } 
+        else
+        {
+            message.innerHTML = "Please enter a valid email address";
+        }
+    }
+    );   
+}
